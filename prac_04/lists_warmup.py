09a5bbacf2ad9a8560443a05b3fd5ea514beec8a
my.name <moverest4821>
2021-12-06 01:34:07 +0800

    Practical 4

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+# Change the first element of number to 10
+numbers[0] = "ten"
+# Change the last element of number to 1
+numbers[-1] = 1
+# Get all the elements from numbers except the first two
+9 in numbers
