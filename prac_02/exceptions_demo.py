1a5c2edf657fe6a4cd18446cff07c6885515aa0e
my.name <moverest4821>
2021-12-06 01:36:30 +0800

    Practical 2

---
+++ prac_02/exceptions_demo.py
@@ -0,0 +1,15 @@
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+"""
+1. When numerator or denominator is not a number.
+2. When the denominator is zero.
+"""
---
+++ prac_02/string_formatting_examples.py
@@ -0,0 +1,7 @@
+0
+50
+100
+150
+
+for number in range(0, 151, 50):
+    print("{:3}".format(number))
