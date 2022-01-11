441d7aaaad6c9627dd2e43b4b16839cc4f068853
my.name <moverest4821>
2022-01-11 19:02:24 +0800

    Practical 8

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,13 @@
+from prac_08.unreliable_car import UnreliableCar
+
+def main():
+    good_car = UnreliableCar("Mainly Good", 100, 90)
+    bad_car = UnreliableCar("Mainly bad", 100, 9)
+    for i in range(1, 12):
+        print("Attempting to drive {}km:".format(i))
+        print("{:12} drove {:2}km".format(good_car.name, good_car.drive(i)))
+        print("{:12} drove {:2}km".format(bad_car.name, bad_car.drive(i)))
+
+    print(good_car)
+    print(bad_car)
+main()
