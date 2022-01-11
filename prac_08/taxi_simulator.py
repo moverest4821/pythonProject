c16ca7957c9dc240fedf50a30925e9b9df7b643f
my.name <moverest4821>
2022-01-11 19:50:57 +0800

    Practical 8

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,69 @@
+from prac_08.car import Car
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
+        else:
+            print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_bill))
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+    print("Total trip cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
+
+
+def run_tests():
+    bus = Car("Datsun", 180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    bus.drive(55)
+    print("fuel =", bus.fuel)
+    print("odo = ", bus.odometer)
+    print(bus)
+
+    distance = int(input("Drive how far? "))
+    while distance > 0:
+        travelled = bus.drive(distance)
+        print("{} travelled {}".format(str(bus), travelled))
+        distance = int(input("Drive how far? "))
+
+    t = Taxi("Prius 1", 100)
+    print(t)
+    t.drive(25)
+    print(t, t.get_fare())
+    t.start_fare()
+    t.drive(40)
+    print(t, t.get_fare())
+
+    sst = SilverServiceTaxi("Limo", 100, 2)
+    print(sst, sst.get_fare())
+    sst.drive(10)
+    print(sst, sst.get_fare())
+
+
+main()
