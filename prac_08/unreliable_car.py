8f6097d1e10dd6a72e3e6690ec414c2431c072a1
my.name <moverest4821>
2022-01-11 18:58:46 +0800

    Practical 8

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,15 @@
+from random import randint
+from Car import Car
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_drive = super().drive(distance)
+        return distance_drive
